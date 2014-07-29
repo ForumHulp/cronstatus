0eac82481c982bdf30d5019a8fba3b84ffcd7ee9
lavigor <lavigor@users.noreply.github.com>
2014-07-29 19:07:26 +0400

    [feature/acp_page] Table view added.

--- acp/cron_status_module.php
+++ acp/cron_status_module.php
@@ -14,7 +14,7 @@ class cron_status_module
 	public $u_action;
 	function main($id, $mode)
 	{
-		global $user, $template, $request, $phpbb_container;
+		global $db, $user, $template, $request, $phpbb_container;
 
 		$action	= $request->variable('action', '');
 
@@ -26,38 +26,61 @@ class cron_status_module
 			break;
 		}
 		
+		$tasks = array();
 		$tasks = $phpbb_container->get('cron.manager')->get_tasks();
 
 		if (empty($tasks))
 		{
 			$template->assign_vars(array(
-				'S_READY_TASKS'		=> false,
-				'L_READY_TASKS'		=> '',
-				'READY_TASKS'		=> '',
-				'L_NOT_READY_TASKS'	=> $user->lang['CRON_STATUS_NO_TASKS'],
-				'NOT_READY_TASKS'	=> '',
+				'S_CRON_TASKS'		=> false,
 				'U_ACTION'			=> $this->u_action,
-				)
-			);
+			));
 			return;
 		}
+
+		$sql = 'SELECT * FROM '.CONFIG_TABLE.' WHERE config_name LIKE "%last_gc"';
+		$result = $db->sql_query($sql);
+		$rows = $db->sql_fetchrowset($result);
+
+		$template->assign_vars(array(
+			'S_CRON_TASKS'		=> (sizeof($tasks)) ? true : false,
+			'U_ACTION'			=> $this->u_action,
+		));
 
 		$not_ready_tasks = array();
 		$ready_tasks = array();
 		foreach ($tasks as $task)
 		{
-			if (!$task->is_ready()) $not_ready_tasks[] = $task->get_name();
-			else $ready_tasks[] = $task->get_name();
+			$task_name = $task->get_name();
+			if(empty($task_name)) continue;
+			$task_date = -1;
+			$find = strpos($task_name, 'tidy');
+			if($find !== false)
+			{
+				$name = substr($task_name, $find + 5);
+				$task_date = (int) $this->array_find($name, $rows);
+			}
+			else if (strpos($task_name, 'prune_notifications'))
+			{
+				$task_date = (int) $this->array_find('read_notifications', $rows);
+			}
+			$template->assign_block_vars(($task->is_ready()) ? 'ready' : 'not_ready', array(
+				'DISPLAY_NAME'	=> $task_name,
+				'TASK_DATE'		=> ($task_date == -1) ? $user->lang['CRON_TASK_AUTO'] : (($task_date) ? $user->format_date($task_date) : $user->lang['CRON_TASK_NEVER_STARTED']),
+			));
 		}
 
-		$template->assign_vars(array(
-			'S_READY_TASKS'		=> (sizeof($ready_tasks)) ? true : false,
-			'L_READY_TASKS'		=> $user->lang['CRON_STATUS_READY_TASKS'],
-			'READY_TASKS'		=> implode('<br />', $ready_tasks),
-			'L_NOT_READY_TASKS'	=> $user->lang['CRON_STATUS_NOT_READY_TASKS'],
-			'NOT_READY_TASKS'	=> implode('<br />', $not_ready_tasks),
-			'U_ACTION'			=> $this->u_action,
-			)
-		);
+		
+	}
+
+	// array_search with partial matches
+	public function array_find($needle, $haystack) 
+	{
+		if(!is_array($haystack)) return false;
+		foreach ($haystack as $key => $item) 
+		{
+			if (strpos($item['config_name'], $needle) !== false) return $haystack[$key]['config_value'];
+		}
+		return false;
 	}
 }
