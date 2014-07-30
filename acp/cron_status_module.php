c13bd83da091b7490b1f6af74519e7da10e7d350
John <info@forumhulp.com>
2014-07-30 08:25:51 +0200

    Refresh and Queue

--- acp/cron_status_module.php
+++ acp/cron_status_module.php
@@ -18,13 +18,15 @@ class cron_status_module
 
 		$this->page_title = $user->lang['ACP_CRON_STATUS_TITLE'];
 		$this->tpl_name = 'acp_cron_status';
-		
+		// Refreshing the page every 60 seconds...
+		meta_refresh(60, $this->u_action);
+
 		$tasks = array();
 		$tasks = $phpbb_container->get('cron.manager')->get_tasks();
 
 		if (sizeof($tasks))
 		{
-			$sql = 'SELECT config_name, config_value FROM '.CONFIG_TABLE.' WHERE config_name LIKE "%gc"';
+			$sql = 'SELECT config_name, config_value FROM '.CONFIG_TABLE.' WHERE config_name LIKE "%gc" OR config_name LIKE "%queue%"';
 			$result = $db->sql_query($sql);
 			$rows = $db->sql_fetchrowset($result);
 			
@@ -56,16 +58,23 @@ class cron_status_module
 				{
 					$task_date = (int) $this->array_find('read_notification_last_gc', $rows);
 					$name = 'read_notification';
+				}
+				else if (strpos($task_name, 'queue'))
+				{
+					$task_date = (int) $this->array_find('last_queue_run', $rows);
+					$name = 'queue';
 				} else
 				{
 					$name = substr($task_name, 15) ;
 					$task_date = (int) $this->array_find($name . '_last_gc', $rows);
 				}
+				$name = ($name == 'queue') ? 'queue_interval' : $name . '_gc';
+			
 				$template->assign_block_vars(($task->is_ready()) ? 'ready' : 'not_ready', array(
 					'DISPLAY_NAME'	=> $task_name,
 					'TASK_DATE'		=> ($task_date == -1) ? $user->lang['CRON_TASK_AUTO'] : (($task_date) ? 
-										$user->format_date($task_date, 'j-m-Y H:m') : $user->lang['CRON_TASK_NEVER_STARTED']),
-					'NEW_DATE'		=> ($task_date) ? $user->format_date($task_date + (int) $this->array_find($name. '_gc', $rows), 'j-m-Y H:m') : '',
+										$user->format_date($task_date, 'j-m-Y H:i') : $user->lang['CRON_TASK_NEVER_STARTED']),
+					'NEW_DATE'		=> ($task_date) ? $user->format_date(($task_date + $this->array_find($name, $rows)), 'j-m-Y H:i') : '',
 				));
 			}
 			$template->assign_vars(array('S_CRON_TASKS' => true));
