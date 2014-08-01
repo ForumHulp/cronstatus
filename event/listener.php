6d8d2657b534de9e56b16de6daa938d26ee305a4
John <info@forumhulp.com>
2014-08-01 22:41:49 +0200

    More improvements

--- event/listener.php
+++ event/listener.php
@@ -110,38 +110,10 @@ class listener implements EventSubscriberInterface
 				'cron_status_main_notice'				=> array('lang' => 'CRON_STATUS_MAIN_NOTICE',	'validate' => 'bool',	'type' => 'radio:yes_no', 'explain' => true),
 				'legend'.($submit_legend_number + 1)	=> 'ACP_SUBMIT_CHANGES',
 			);
-			$display_vars['vars'] = $this->insert_config_array($display_vars['vars'], $new_vars, array('after' => $submit_key));
+			$display_vars['vars'] = insert_config_array($display_vars['vars'], $new_vars, array('after' => $submit_key));
 			$event['display_vars'] = $display_vars;
 		}
 	}
-
-	/**
-	 * Inserts new config display_vars into an exisiting display_vars array
-	 * at the given position.
-	 * NOTE: this function is temporary until an official one will come out.
-	 *
-	 * @param array $display_vars An array of existing config display vars
-	 * @param array $add_config_vars An array of new config display vars
-	 * @param array $where Where to place the new config vars,
-	 *              before or after an exisiting config, as an array
-	 *              of the form: array('after' => 'config_name') or
-	 *              array('before' => 'config_name').
-	 * @return array The array of config display vars
-	 */
-	public function insert_config_array($display_vars, $add_config_vars, $where)
-	{
-		if (is_array($where) && array_key_exists(current($where), $display_vars))
-		{
-			$position = array_search(current($where), array_keys($display_vars)) + ((key($where) == 'before') ? 0 : 1);
-			$display_vars = array_merge(
-				array_slice($display_vars, 0, $position),
-				$add_config_vars,
-				array_slice($display_vars, $position)
-			);
-		}
-		return $display_vars;
-	}
-
 
 	public function load_language_on_setup($event)
 	{
