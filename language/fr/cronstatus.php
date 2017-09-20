4c424bc8781302055e468103a95801c5347aec5e
Raphaël M <Galixte@users.noreply.github.com>
2017-09-20 15:59:44 +0200

    Update of the FRENCH for the Cron Status extension

--- language/fr/cronstatus.php
+++ language/fr/cronstatus.php
@@ -1,13 +1,17 @@
 <?php
 /**
-*
-* @package cronstatus
-* @copyright (c) 2014 John Peskens (http://ForumHulp.com) and Igor Lavrov (https://github.com/LavIgor)
-* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
-* French translation by Galixte (http://www.galixte.com)
-*
-*/
+ *
+ * Cron Status. An extension for the phpBB Forum Software package.
+ * French translation by Galixte (http://www.galixte.com)
+ *
+ * @copyright (c) 2017 John Peskens (http://ForumHulp.com) and Igor Lavrov (https://github.com/LavIgor)
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
 
+/**
+ * DO NOT CHANGE
+ */
 if (!defined('IN_PHPBB'))
 {
 	exit;
@@ -18,33 +22,48 @@ if (empty($lang) || !is_array($lang))
 	$lang = array();
 }
 
+// DEVELOPERS PLEASE NOTE
+//
+// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
+//
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
+//
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+//
+// Some characters you may want to copy&paste:
+// ’ « » “ ” …
+//
+
 $lang = array_merge($lang, array(
 	'CRON'								=> 'Cron',
 	'CRON_LOCKED'						=> 'Cron arrêté',
 	'CRON_TIME_LOCKED'					=> 'Temps d’arrêt du Cron',
 	'ACP_CRON_STATUS_TITLE'				=> 'Statut du Cron',
-	'ACP_CRON_STATUS_EXPLAIN'			=> 'Voici la page de votre forum phpBB où vous pouvez vérifier le statut des taches du Cron. L’indication "Automatique" concernant la dernière date d’une tâche signifie que la tâche a une option dédiée à la gestion de son temps qui ne peut être reconnue par l’extension « Cron Status ». Une tâche indiquée en rouge signifie qu’elle n’a jamais été exécutée ou qu’il y a un problème. Un verrou rouge signifie que cette tâche est verrouillée par le gestionnaire du Cron et bloque d’autres tâches.',
+	'ACP_CRON_STATUS_EXPLAIN'			=> 'Depuis cette page il est possible d’avoir une vue d’ensemble sur le statut des tâches du Cron. Une tâche marquée en rouge signifie que celle-ci n’a jamais été exécutée ou rencontre un problème. Un verrou rouge signifie que celle-ci est en cours d’exécution, ou que celle-ci est verrouillée par le gestionnaire du Cron et bloque d’autres tâches.<br /><br />Réinitialiser le verrouillage du Cron en cliquant sur le verrou.<br /><br />Exécuter une tâche disponible en cliquant sur « Exécuter la tâche maintenant » situé à droite de son nom.<br /><br />L’indication « Automatique » concerne une tâche qui possède sa propre gestion de sa fréquence d’exécution et qui ne peut être prise en charge par l’extension « Cron Status ».',
 	'CRON_STATUS_REFRESH'				=> 'Nombre de secondes avant l’actualisation',
 	'CRON_TASK_LOCKED'					=> 'Tâche du Cron arrêtée',
 	'CRON_STATUS_READY_TASKS'			=> 'Tâches disponibles',
 	'CRON_STATUS_NOT_READY_TASKS'		=> 'Tâches non disponibles',
-	'CRON_STATUS_NO_TASKS'				=> 'Aucune tâche du Cron disponible',
+	'CRON_STATUS_NO_TASKS'				=> 'Aucune tâche du Cron n’est prête à être exécutée',
 	'CRON_STATUS_DATE_FORMAT'			=> 'Format de la date du statut des tâches du Cron',
 	'CRON_STATUS_DATE_FORMAT_EXPLAIN'	=> 'Le format de la date est le même que la fonction <code>date</code> de PHP.',
-	'CRON_STATUS_MAIN_NOTICE'			=> 'Notification sur la page de l’index de l’administration',
-	'CRON_STATUS_MAIN_NOTICE_EXPLAIN'	=> 'Si le Cron est arrêté une notification est affichée sur le statut du Cron sur la page de l’index de l’administration.',
-	'CRON_STATUS_RUN_ALWAYS'			=> 'Run cron always',
-	'CRON_STATUS_RUN_ALWAYS_EXPLAIN'	=> 'Run cron jobs also triggered by bots. Works great for forums not that busy.',
+	'CRON_STATUS_MAIN_NOTICE'			=> 'Notification dans le panneau d’administration',
+	'CRON_STATUS_MAIN_NOTICE_EXPLAIN'	=> 'Permet d’être informé lorsque le Cron est arrêté par une notification affichée sur la page de l’index du panneau d’administration.',
+	'CRON_STATUS_RUN_ALWAYS'			=> 'Exécuter le Cron plus souvent',
+	'CRON_STATUS_RUN_ALWAYS_EXPLAIN'	=> 'Permet d’exécuter les tâches du Cron lors des visites des robots. Ce comportement donne de bons résultats sur les forums peu fréquentés.',
 	'CRON_TASK_NAME'					=> 'Nom de la tâche',
-	'CRON_TASK_DATE'					=> 'Date de la dernière exécution',
-	'CRON_NEW_DATE'						=> 'Date de la prochaine exécution',
+	'CRON_TASK_DATE'					=> 'Dernière exécution',
+	'CRON_NEW_DATE'						=> 'Prochaine exécution',
 	'CRON_TASK_NEVER_STARTED'			=> 'N’a jamais été exécutée',
 	'CRON_TASK_AUTO'					=> 'Automatique',
 	'CRON_TASK_DATE_TIME'				=> 'Date et heure actuelles',
 	'CRON_STATUS_ERROR'					=> 'Erreur d’actualisation',
 	'CRON_STATUS_TIMEOUT'				=> 'Temps d’actualisation dépassé',
 	'CRON_STATUS_ERROR_EXPLAIN'			=> 'Une erreur s’est produite lors de l’actualisation de la page.',
-	'CRON_STATUS_DEVELOPERS'			=> 'Développeurs',
-	'CRON_TASK_RUN'						=> 'Exécuter',
-	'CRON_TASK_RUNNING'					=> 'En cours d’exécution ...',
+	'CRON_TASK_RUN'						=> 'Exécuter la tâche maintenant',
+	'CRON_TASK_RUNNING'					=> 'En cours d’exécution…',
 ));
--- language/fr/info_acp_cronstatus.php
+++ language/fr/info_acp_cronstatus.php
@@ -1,13 +1,17 @@
 <?php
 /**
-*
-* @package cronstatus
-* @copyright (c) 2014 John Peskens (http://ForumHulp.com) and Igor Lavrov (https://github.com/LavIgor)
-* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
-* French translation by Galixte (http://www.galixte.com)
-*
-*/
+ *
+ * Cron Status. An extension for the phpBB Forum Software package.
+ * French translation by Galixte (http://www.galixte.com)
+ *
+ * @copyright (c) 2017 John Peskens (http://ForumHulp.com) and Igor Lavrov (https://github.com/LavIgor)
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
 
+/**
+ * DO NOT CHANGE
+ */
 if (!defined('IN_PHPBB'))
 {
 	exit;
@@ -18,32 +22,48 @@ if (empty($lang) || !is_array($lang))
 	$lang = array();
 }
 
+// DEVELOPERS PLEASE NOTE
+//
+// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
+//
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
+//
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+//
+// Some characters you may want to copy&paste:
+// ’ « » “ ” …
+//
+
 $lang = array_merge($lang, array(
-	'ACP_CRON_STATUS_TITLE'	=> 'Statut  du Cron',
-	'LOG_CORE_INSTALLED'	=> 'Files succesfully changed',
-	'LOG_CORE_DEINSTALLED'	=> 'Files succesfully changed back',
-	'LOG_CORE_NOT_REPLACED'	=> 'Could not replaced file(s)<br />» %s',
-	'LOG_CORE_NOT_UPDATED'	=> 'Could not update file(s)<br />» %s',
-	'FH_HELPER_NOTICE'		=> 'Forumhulp helper application does not exist!<br />Download <a href="https://github.com/ForumHulp/helper" target="_blank">forumhulp/helper</a> and copy the helper folder to your forumhulp extension folder.',
-	'CRONSTAT_NOTICE'		=> '<div class="phpinfo"><p class="entry">This extension resides in %1$s » %2$s » %3$s.</p></div>',
+	'ACP_CRON_STATUS_TITLE'	=> 'Statut du Cron',
+	'LOG_CORE_INSTALLED'	=> 'Fichiers modifiés avec succès !',
+	'LOG_CORE_DEINSTALLED'	=> 'Fichiers restaurés avec succès !',
+	'LOG_CORE_NOT_REPLACED'	=> 'Impossible de remplacer les fichiers<br />» %s',
+	'LOG_CORE_NOT_UPDATED'	=> 'Impossible de mettre à jour les fichiers<br />» %s',
+	'FH_HELPER_NOTICE'		=> 'L’extension « Forumhulp Helper » n’est pas installée !<br />Merci de la télécharger depuis la page « <a href="https://github.com/ForumHulp/helper" target="_blank">forumhulp/helper</a> », puis de l’installer.',
+	'CRONSTAT_NOTICE'		=> '<div class="phpinfo" style="max-width:556px;margin-right:auto;margin-left:auto;"><p class="entry"><br />Cette extension est disponible depuis la page : %1$s » %2$s » %3$s.</p></div>',
 ));
 
 // Description of Cron Status extension
 $lang = array_merge($lang, array(
 	'DESCRIPTION_PAGE'		=> 'Description',
-	'DESCRIPTION_NOTICE'	=> 'Extension note',
+	'DESCRIPTION_NOTICE'	=> 'Informations sur l’extension',
 	'ext_details' => array(
 		'details' => array(
-			'DESCRIPTION_1'		=> 'Vue d’ensemble des tâches du Cron (avec tri).',
-			'DESCRIPTION_2'		=> 'Affiche le statut de chaque tâche du Cron.',
-			'DESCRIPTION_3'		=> 'Vous pouvez exécuter manuellement n’importe quelle tâche disponible.',
-			'DESCRIPTION_4'		=> 'Reset cronlock with a simple click',
+			'DESCRIPTION_1'	=> 'Vue d’ensemble des tâches du Cron (avec tri).',
+			'DESCRIPTION_2'	=> 'Affichage du statut de chaque tâche du Cron.',
+			'DESCRIPTION_3'	=> 'Exécution manuelle de n’importe quelle tâche disponible.',
+			'DESCRIPTION_4'	=> 'Réinitialisation du verrouillage du Cron en un clic.',
 		),
 		'note' => array(
-			'NOTICE_1'			=> 'Notification sur le statut du Cron (optionnelle)',
-			'NOTICE_2'			=> 'Peut être désactivée dans la page « Configuration du forum.',
-			'NOTICE_3'			=> 'Switchable Run Cron Always',
-			'NOTICE_4'			=> 'phpBB 3.2 ready'
+			'NOTICE_1'		=> 'Notification sur le statut du Cron (optionnelle).',
+			'NOTICE_2'		=> 'Peut être désactivée depuis la page « Configuration du forum ».',
+			'NOTICE_3'		=> 'Permet d’exécuter le Cron plus souvent.',
+			'NOTICE_4'		=> 'Compatible avec phpBB 3.2.x.'
 		)
 	)
 ));
